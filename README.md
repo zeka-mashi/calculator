66a3fd388a6d710a6c2308eac59ee460b35a5faa
David <lin.david32@gmail.com>
2022-04-13 15:20:20 -0700

    add input limit logic and error line

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mini Calculator
+
+A calculator project as part of TOP course.
+
+## Features
+
+- Support for all 10 digits, float numbers, five operations, with a delete and clear function
+- Automatically calculates on the fly with dual-line display
+- Prevents unsafe and overflowing input sizes
+- Keyboard support for all of the calculator's functionality
+
+## Demo
+
+Demo this calculator [here](https://zeka-mashi.github.io/mini-calculator/).
+
+## FAQ
+
+#### Is there PEMDAS support?
+
+For simplicity's sake, there is no PEMDAS (order of operations) support. Operations are handled left to right.
+
+#### What keyboard keys are supported?
+
+Digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9    
+Operations: %, /, *, +, -   
+Misc: Backspace, Esc, . (period), =
--- script.js
+++ script.js
@@ -16,12 +16,20 @@ function updateDisplay() {
                 continue
             }
         }
+        full_arr = merged_arr.join("");
+        if (full_arr.length >= 20) {
+            const elmMid = document.getElementById("mid");
+            elmMid.textContent = "Input too large!";
+            del();
+        }
         if (calc_nums.length > 1 && calc_nums.length > calc_ops.length) {
             var results = calc_nums[0];
             for (let i = 0; i < calc_ops.length; i++) {
                 expr = results + calc_ops[i] + calc_nums[i + 1];
                 if (calc_ops[i] === "/" && parseInt(calc_nums[i + 1]) == 0) {
-                    results = "ERROR!";
+                    const elmMid = document.getElementById("mid");
+                    elmMid.textContent = "Illegal calculation!";
+                    results = "NaN";
                     break;
                 } else {
                     results = Function('return ' + expr)();
@@ -35,6 +43,11 @@ function updateDisplay() {
         }
     } else {
         merged_arr = calc_nums;
+        if (merged_arr.length > 0 && merged_arr[0].length >= 20) {
+            const elmMid = document.getElementById("mid");
+            elmMid.textContent = "Input too large!";
+            del();
+        }
     }
     elmTop.textContent = merged_arr.join("");
     if (calc_nums.length == 0) {
@@ -47,6 +60,7 @@ function updateDisplay() {
 
 function clear() {
     const elmTop = document.getElementById("top");
+    const elmMid = document.getElementById("mid");
     const elmBot = document.getElementById("bottom");
     calc_nums = [];
     calc_ops = [];
@@ -59,10 +73,15 @@ function clear() {
         }
     }
     elmTop.textContent = "";
+    elmMid.textContent = "";
     elmBot.textContent = "";
 }
 
-function del() {
+function del(e) {
+    if (e) {
+        const elmMid = document.getElementById("mid");
+        elmMid.textContent = "";
+    }
     if (calc_nums.length == calc_ops.length) {
         calc_ops.pop();
     } else if (calc_nums.length > 0) {
@@ -77,6 +96,8 @@ function del() {
 }
 
 function addOperation(op) {
+    const elmMid = document.getElementById("mid");
+    elmMid.textContent = "";
     if (calc_nums.length !== calc_ops.length) {
         calc_ops.push(op);
     } else if (calc_ops[calc_ops.length - 1] !== op) {
@@ -89,14 +110,19 @@ function addValue(num) {
     if (calc_nums.length == calc_ops.length) {
         calc_nums.push(num);
     } else {
-        var last_item = calc_nums[calc_nums.length - 1];
-        calc_nums[calc_nums.length - 1] = last_item + num;
+        var last_item = calc_nums[calc_nums.length - 1] + num;
+        if (last_item < Number.MAX_SAFE_INTEGER && last_item > Number.MIN_SAFE_INTEGER) {
+            calc_nums[calc_nums.length - 1] = last_item;
+        } else {
+            const elmMid = document.getElementById("mid");
+            elmMid.textContent = "Number out of range!";
+        }
     }
     updateDisplay();
 }
 
 function addDecimal() {
-    if (calc_nums.length > 0) {
+    if (calc_nums.length > calc_ops.length) {
         var last_item = calc_nums[calc_nums.length - 1].toString();
         if (last_item.indexOf(".") == -1) {
             calc_nums[calc_nums.length - 1] = last_item + ".";
@@ -105,10 +131,24 @@ function addDecimal() {
     updateDisplay();
 }
 
+function throwError() {
+    const buttons = document.getElementsByClassName("btn");
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].dataset.action !== "clear") {
+            buttons[i].style.pointerEvents = 'none';
+            buttons[i].style.backgroundColor = "rgb(183, 183, 183)";
+            buttons[i].style.color = "rgb(150, 150, 150)";
+        }
+    }
+}
+
 function evaluate() {
+    const elmMid = document.getElementById("mid");
+    elmMid.textContent = ""
     const elmBot = document.getElementById("bottom");
     if (calc_nums.length == calc_ops.length) {
-        elmBot.textContent = "ERROR!";
+        elmMid.textContent = "Missing arguments!";
+        elmBot.textContent = "NaN";
     } else if (calc_nums.length === 1) {
         elmBot.textContent = calc_nums[0];
     } else if (calc_nums.length > 1 && calc_nums.length > calc_ops.length) {
@@ -116,15 +156,9 @@ function evaluate() {
         for (let i = 0; i < calc_ops.length; i++) {
             expr = results + calc_ops[i] + calc_nums[i + 1];
             if (calc_ops[i] === "/" && parseInt(calc_nums[i + 1]) == 0) {
-                results = "ERROR!";
-                const buttons = document.getElementsByClassName("btn");
-                for (let i = 0; i < buttons.length; i++) {
-                    if (buttons[i].dataset.action !== "clear") {
-                        buttons[i].style.pointerEvents = 'none';
-                        buttons[i].style.backgroundColor = "rgb(183, 183, 183)";
-                        buttons[i].style.color = "rgb(150, 150, 150)";
-                    }
-                }
+                elmMid.textContent = "Illegal calculation!";
+                results = "NaN";
+                throwError();
                 break;
             } else {
                 results = Function('return ' + expr)();
@@ -136,6 +170,9 @@ function evaluate() {
             results = results;
         }
         elmBot.textContent = results;
+        if (results === "NaN") {
+            throwError();
+        }
         calc_nums = [];
         calc_nums.push(results);
         calc_ops = [];
@@ -165,7 +202,6 @@ document.addEventListener("DOMContentLoaded", function() {
 }, false);
 
 document.addEventListener("keydown", function(e) {
-    console.log(e);
     if (e.key === "Escape") {
         clear();
     } else if (e.key === "Backspace") {
